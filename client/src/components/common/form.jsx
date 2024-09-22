a3fd8b25b10d90ca425b45584457817cb4dfd3c8
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-09-22 19:22:06 +0500

    fourth commit

---
+++ client/src/components/common/form.jsx
@@ -0,0 +1,88 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+function CommonForm({ formControls }) {
+  function renderInputsByComponentType(getControlItem) {
+    let element = null;
+    switch (getControlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+
+      case "select":
+        element = (
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {getControlItem.options && getControlItem.options.length > 0
+                ? getControlItem.options.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+
+        break;
+
+      case "textarea":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+
+      default:
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+    }
+    return element;
+  }
+
+  return (
+    <form>
+      <div className="fex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
+            <Label className="mb-1">{controlItem.label}</Label>
+            {renderInputsByComponentType(controlItem)}
+          </div>
+        ))}
+      </div>
+    </form>
+  );
+}
+
+export default CommonForm;
