4a66b730d71df8e2c2658ddf01bf56febb2403bf
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-09-23 23:57:46 +0500

    toaster added in register form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import store from "../src/store/store.js";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </BrowserRouter>
 );
