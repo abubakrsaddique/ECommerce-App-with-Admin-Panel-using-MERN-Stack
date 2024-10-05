2b64ef423f0aa2b2005d0c196e2c78503cd72e80
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-10-05 22:31:22 +0500

    vercel.json updated

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
